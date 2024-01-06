1ea77ef3f9462a25a81dc25f9272fb0617ce9615
Rommyaji <rommylaksana@gmail.com>
2024-01-06 19:12:18 +0700

    give token on cookie

--- utils/auth.js
+++ utils/auth.js
@@ -1,14 +1,17 @@
 const jwt = require('jsonwebtoken')
 
 const verifyToken = (req, res, next) => {
-  const authHeader = req.headers['authorization']
-  const token = authHeader && authHeader.split(' ')[1]
-  if(token == null) return res.sendStatus(401)
-  jwt.verify(token, process.env.API_SECRET, (err, decoded) => {
-    if (err) return res.sendStatus(403)
-    req.Name = decoded.Name
-    next()
-  })
+
+  const token = req.cookie.access_token
+  if(!token || token === null) {
+      return res.sendStatus(401)
+  } else {
+      jwt.verify(token, process.env.API_SECRET, (err, decoded) => {
+        if (err) return res.sendStatus(403)
+        req.Name = decoded.Name
+        next()
+    })
+  }
 }
 
 
